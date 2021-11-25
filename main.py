896908dcd6a3ac51a9eb72799bbb144cd2e1285f
Jonathan Lopez <jonathanlop82@gmail.com>
2021-11-24 20:08:42 -0500

    Update and delete in mysql

--- main.py
+++ main.py
@@ -19,6 +19,12 @@ SHOW_TABLES = "SHOW TABLES"
 
 INSERT_USER = "INSERT INTO users (username , password) VALUES ( '{username}','{password}')"
 
+SELECT_USER = "SELECT * FROM users WHERE id = {id}"
+
+UPDATE_USER = "UPDATE users SET username='{username}', password='{password}' WHERE id = {id} "
+
+DELETE_USER = "DELETE FROM users WHERE id = {id}"
+
 def run():
     try:
         #Para conectar a la base de datos
@@ -29,23 +35,48 @@ def run():
         #cursor.execute(USER_TABLE)
 
         # cursor.execute(SHOW_TABLES)
-        # #Para mostrar las tablas en la pantalla
+        ###Para mostrar las tablas en la pantalla
         # tables = cursor.fetchall()  
         # # Se recorre las tablas 
         # for table in tables:
         #     print(table[0])
 
-        username = input("ingrese el username ")
-        password = input("ingrese el password ")
-        #Insertar una tupla en la tabla
-        query = INSERT_USER.format(username=username, password=password)
-        print(query)
+        #username = input("ingrese el username ")
+        #password = input("ingrese el password ")
+        
+        
+        ####Insertar una tupla en la tabla
+        # query = INSERT_USER.format(username=username, password=password)
+        # print(query)
+
+        # try:
+        #     cursor.execute(query)
+        #     connection.commit()
+        # except: #Si ocurre algun error durante la insercion
+        #     connection.rollback()
+
+        query = UPDATE_USER.format(username='codigofacilito', password='123', id=1)
+
+        
+        try:
+            cursor.execute(query)
+            connection.commit()
+        except:
+            connection.rollback()
+
+        query = DELETE_USER.format(id=2)
 
         try:
             cursor.execute(query)
             connection.commit()
-        except: #Si ocurre algun error durante la insercion
+        except:
             connection.rollback()
+        
+        query = SELECT_USER.format(id=1)
+        cursor.execute(query)
+        users = cursor.fetchall()
+        for user in users:
+            print(user)
 
         connection.close()
     except MySQLdb.Error as error:
