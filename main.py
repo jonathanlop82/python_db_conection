7fd0c8d2cf925d54e53caf736214859def1b3368
Jonathan Lopez <jonathanlop82@gmail.com>
2021-11-24 19:48:24 -0500

    insert to db

--- main.py
+++ main.py
@@ -17,21 +17,35 @@ DROP_USER = " DROP TABLE IF EXISTS `users`"
 
 SHOW_TABLES = "SHOW TABLES"
 
+INSERT_USER = "INSERT INTO users (username , password) VALUES ( '{username}','{password}')"
+
 def run():
     try:
         #Para conectar a la base de datos
         connection = MySQLdb.connect(HOST, USER, PASSWORD, DATABASE, PORT)
 
         cursor = connection.cursor()
-        cursor.execute(DROP_USER)
-        cursor.execute(USER_TABLE)
+        #cursor.execute(DROP_USER)
+        #cursor.execute(USER_TABLE)
 
-        cursor.execute(SHOW_TABLES)
-        #Para mostrar las tablas en la pantalla
-        tables = cursor.fetchall()  
-        # Se recorre las tablas 
-        for table in tables:
-            print(table[0])
+        # cursor.execute(SHOW_TABLES)
+        # #Para mostrar las tablas en la pantalla
+        # tables = cursor.fetchall()  
+        # # Se recorre las tablas 
+        # for table in tables:
+        #     print(table[0])
+
+        username = input("ingrese el username ")
+        password = input("ingrese el password ")
+        #Insertar una tupla en la tabla
+        query = INSERT_USER.format(username=username, password=password)
+        print(query)
+
+        try:
+            cursor.execute(query)
+            connection.commit()
+        except: #Si ocurre algun error durante la insercion
+            connection.rollback()
 
         connection.close()
     except MySQLdb.Error as error:
